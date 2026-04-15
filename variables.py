907532dbaaff46ba8c48d3802ccfdb2cd93b0fb1
Srujan2023 <Srujan2004ab@gmail.com>
2026-04-15 12:20:00 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+#variables in python
+a=10
+b=20
+print("a is:",a)
+print("b is:",b)
+
+#type of variables
+print("type of a is:",type(a))
+
+#float variable
+c=3.14
+print("c is:",c)
+print("type of c is:",type(c))
+
+#string variable
+name="python programming"
+print("name is:",name)
+print("type of name is:",type(name))
+
